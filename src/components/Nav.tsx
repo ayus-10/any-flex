9b07e3a6afdc08311ad20ad77022d6e952654bbd
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-02-25 13:29:42 +0545

    Implemented API fetch to display genre names on Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,13 @@
 import { MdAccountCircle } from "react-icons/md";
-import { CiSearch } from "react-icons/ci";
 import Link from "next/link";
 import { useState } from "react";
 
-export default function Nav() {
+export default function Nav({ NavData }: { NavData: string[] }) {
   const [showDropdown, setShowDropdown] = useState(false);
 
-  const genreList = [
-    "Action",
-    "Adventure",
-    "Comedy",
-    "Drama",
-    "Fantasy",
-    "Horror",
-  ];
+  const [search, setSearch] = useState("");
+
+  const genreList = NavData;
 
   return (
     <>
@@ -38,7 +32,7 @@ export default function Nav() {
               Genre
             </span>
             <ul
-              className={`absolute left-1/2 top-[2.5rem] z-30 flex -translate-x-1/2 flex-col gap-2 rounded-lg border-[1px] border-zinc-700 bg-zinc-800 p-3 ${showDropdown ? "block" : "hidden"}`}
+              className={`absolute left-1/2 top-[2.5rem] z-30 flex max-h-48 -translate-x-1/2 flex-col gap-2 overflow-y-scroll rounded-lg border-[1px] border-zinc-700 bg-zinc-800 p-3 md:max-h-72 ${showDropdown ? "block" : "hidden"}`}
               onMouseOver={() => setShowDropdown(true)}
               onMouseOut={() => setShowDropdown(false)}
             >
@@ -60,25 +54,13 @@ export default function Nav() {
         </Link>
       </nav>
       <div className="sticky inset-0 z-10 w-full border-b-[1px] border-zinc-600 bg-zinc-900 px-4 py-5 text-white">
-        <div className="mx-auto flex max-w-[1280px] items-center justify-between">
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Search"
-              className="rounded-full bg-zinc-800 p-2 opacity-80 outline-none duration-200 ease-in-out focus:opacity-100 md:p-3"
-            />
-            <div className="absolute right-2 top-1/2 -translate-y-1/2 md:right-4">
-              <CiSearch />
-            </div>
-          </div>
-          <div>
-            <Link
-              href={"/discover"}
-              className="cursor-pointer text-gray-300 duration-200 ease-in-out hover:text-white"
-            >
-              Discover
-            </Link>
-          </div>
+        <div className="mx-auto flex max-w-[1280px] items-center justify-center">
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="Search"
+            className="w-full rounded-full bg-zinc-800 p-2 opacity-80 outline-none duration-200 ease-in-out focus:opacity-100 md:p-3"
+          />
         </div>
       </div>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import Library from "@/components/Library";
 import Nav from "@/components/Nav";
 import Head from "next/head";
 
-export type ItemData = {
+export type AnimeData = {
   name: string;
   image: string;
   genre: string[];
 };
 
-type ResponseData = {
+type FetchedAnimeData = {
   title: string;
   images: {
     jpg: {
@@ -20,36 +20,61 @@ type ResponseData = {
   }[];
 };
 
+type FetchedGenreList = {
+  data: { name: string }[];
+};
+
 export async function getStaticProps() {
-  const response = await fetch("https://api.jikan.moe/v4/top/anime");
-  const parsedResponse = await response.json();
-  const responseData = parsedResponse.data;
+  // Fetch JSON data to be displayed on Library component
+  const animeDataRaw = await fetch("https://api.jikan.moe/v4/top/anime");
+  const animeDataParsed = await animeDataRaw.json();
+  const animeData = animeDataParsed.data;
 
-  const dataArray: ItemData[] = [];
+  // Empty array to store processed anime data
+  const animeDataArray: AnimeData[] = [];
 
-  responseData.forEach((data: ResponseData) => {
-    const dataObject: ItemData = { name: "", image: "", genre: [] };
+  // Process the fetched data and push it to previously created array
+  animeData.forEach((data: FetchedAnimeData) => {
+    const dataObject: AnimeData = { name: "", image: "", genre: [] };
     dataObject.name = data.title;
     dataObject.image = data.images.jpg.large_image_url;
     data.genres.forEach((genre) => {
       dataObject.genre.push(genre.name);
     });
-    dataArray.push(dataObject);
+    animeDataArray.push(dataObject);
+  });
+
+  // Fetch JSON data containing the genre names
+  const genreListRaw = await fetch("https://api.jikan.moe/v4/genres/anime");
+  const genreListParsed: FetchedGenreList = await genreListRaw.json();
+
+  // Empty array to store processed genre names
+  const genreList: string[] = [];
+
+  // Process and store genre names to previously created array
+  genreListParsed.data.forEach((genre) => {
+    genreList.push(genre.name);
   });
 
   return {
-    props: { LibraryData: dataArray },
+    props: { LibraryData: animeDataArray, NavData: genreList },
   };
 }
 
-export default function Home({ LibraryData }: { LibraryData: ItemData[] }) {
+export default function Home({
+  LibraryData,
+  NavData,
+}: {
+  LibraryData: AnimeData[];
+  NavData: string[];
+}) {
   return (
     <>
       <Head>
         <title>AnyFlex - Flex your favorite anime</title>
         <meta name="description" content="Flex your favorite anime"></meta>
       </Head>
-      <Nav />
+      <Nav NavData={NavData} />
       <Library content={LibraryData} />
     </>
   );
