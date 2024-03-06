3f7429b23d471d537c05a49bff6ccbe89ced7f0e
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-03-06 18:23:42 +0545

    Improved API logic for creating and updating data

--- src/pages/api/addToLibrary.ts
+++ src/pages/api/addToLibrary.ts
@@ -4,10 +4,12 @@ import User from "../../../database/models/User";
 
 export type UserModel = {
   username: string;
-  animeLibrary: {
-    animeId: number;
-    episodesCompleted: number;
-  }[];
+  animeLibrary: AnimeLibrary[];
+};
+
+type AnimeLibrary = {
+  animeId: number;
+  episodesCompleted: number;
 };
 
 export default async function handler(
@@ -15,12 +17,62 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   const reqBody: UserModel = req.body;
+
   connectDB();
-  try {
-    const user = new User(reqBody);
-    await user.save();
-    res.status(201).send("Sucessfully added to library");
-  } catch (error) {
-    res.status(400).json({ error });
+
+  // Get username from request body and query database if user already exists
+  const { username } = reqBody;
+  const existingLibrary: UserModel | null = await User.findOne({ username });
+
+  if (!existingLibrary) {
+    // User entry does not exist, so create new one
+    try {
+      const user = new User(reqBody);
+      await user.save();
+      res.status(201).send("Successfully added to library");
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  } else {
+    // User entry already exist, so update previous one
+    const existingAnimeLibrary: AnimeLibrary[] = existingLibrary.animeLibrary;
+    const newAnimeLibrary: AnimeLibrary[] = reqBody.animeLibrary;
+
+    if (newAnimeLibrary.length !== 1) {
+      res.status(400).send("Can only update one entry at a time");
+      return;
+    }
+
+    const newAnimeLibraryElement = newAnimeLibrary[0]; // Store the first member of request body in a variable
+
+    let updatedAnimeLibrary: AnimeLibrary[] = []; // Initialize an empty array to store the updated data
+
+    let existingAnimeLibraryUpdated = false; // Used to keep track if existing array was updated
+
+    // Iterate over existing array to check and update if newAnimeLibraryElement already exists there
+    for (let index = 0; index < existingAnimeLibrary.length; index++) {
+      if (
+        existingAnimeLibrary[index].animeId === newAnimeLibraryElement.animeId
+      ) {
+        existingAnimeLibrary[index] = newAnimeLibraryElement;
+        updatedAnimeLibrary = [...existingAnimeLibrary];
+        existingAnimeLibraryUpdated = true;
+        break;
+      }
+    }
+
+    if (!existingAnimeLibraryUpdated) {
+      updatedAnimeLibrary = [...existingAnimeLibrary, ...newAnimeLibrary];
+    }
+
+    try {
+      await User.updateOne(
+        { username },
+        { $set: { animeLibrary: updatedAnimeLibrary } },
+      );
+      res.status(200).send("Successfully updated the library");
+    } catch (error) {
+      res.status(400).json({ error });
+    }
   }
 }
