1a9c3df2675774eb7edc60e5004f4ea9432e2663
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-03-07 22:14:35 +0545

    Create page for route /library

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,16 @@
+type ProgressBarProps = {
+  total: number;
+  completed: number;
+};
+
+export default function ProgressBar({ total, completed }: ProgressBarProps) {
+  const completedPercentage = (completed / total) * 100;
+  return (
+    <div className="relative ml-auto h-4 w-full rounded-sm bg-zinc-500">
+      <div
+        className="absolute right-0 h-full rounded-sm bg-[#29D]"
+        style={{ width: `${completedPercentage}%` }}
+      ></div>
+    </div>
+  );
+}
--- src/pages/library/[slug].tsx
+++ src/pages/library/[slug].tsx
@@ -2,9 +2,12 @@ import { useSession, signIn } from "next-auth/react";
 import { FaDiscord } from "react-icons/fa";
 import Nav from "@/components/Nav";
 import Head from "next/head";
+import Image from "next/image";
 import { GetServerSideProps } from "next";
 import axios from "axios";
 import { UserModel } from "../api/addToLibrary";
+import ProgressBar from "@/components/ProgressBar";
+import { TbFaceIdError } from "react-icons/tb";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   try {
@@ -13,11 +16,12 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       `${process.env.BASE_URL}api/retrieveLibrary`,
       { username },
     );
-    const { data }: { data: UserModel } = response;
+    const libraryData: UserModel = response.data.data;
     return {
-      props: { libraryData: data },
+      props: { libraryData },
     };
-  } catch {
+  } catch (e) {
+    console.log(e);
     return {
       notFound: true,
     };
@@ -35,12 +39,54 @@ export default function Library({ libraryData }: { libraryData: UserModel }) {
         <title>My Library - AnyFlex</title>
       </Head>
       <Nav showNavigationAndSearch={false} />
-      <main className="min-h-screen w-full bg-zinc-700">
+      <main className="min-h-screen w-full bg-zinc-700 text-white">
         {status === "authenticated" ? (
-          <div></div>
+          <div className="flex w-full flex-col items-center gap-4 py-4 md:py-8">
+            {animeLibrary.length > 0 ? (
+              animeLibrary.map((anime) => (
+                <div className="flex w-full justify-between border-y-[1px] border-zinc-500 bg-zinc-900 md:w-[90%] lg:w-[75%]">
+                  <div className="relative min-h-[150px] min-w-[100px] md:min-h-[200px] md:min-w-[150px]">
+                    <Image
+                      fill
+                      className="object-cover"
+                      src={anime.imageURL}
+                      alt={`Image for ${anime.animeName}`}
+                    ></Image>
+                  </div>
+                  <div className="flex grow flex-col justify-evenly px-4 py-2 md:py-0">
+                    <h2 className="text-right text-lg font-semibold md:text-2xl md:font-bold">
+                      {anime.animeName}
+                    </h2>
+                    <div className="text-lg italic text-neutral-400 md:text-xl">
+                      <ProgressBar
+                        total={anime.totalEpisodes}
+                        completed={anime.episodesCompleted}
+                      />
+                      <p className="text-right font-medium">
+                        Completed {anime.episodesCompleted} out of{" "}
+                        {anime.totalEpisodes} episodes
+                      </p>
+                    </div>
+                    <button className="ml-auto rounded-sm border-2 border-[#29D] bg-[#29D] px-2 text-lg font-bold duration-200 ease-in-out hover:bg-transparent md:px-3 md:py-1">
+                      Edit
+                    </button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="grid w-full place-content-center py-8">
+                <div className="flex flex-col items-center justify-center px-4">
+                  <TbFaceIdError className="text-9xl" />
+                  <h1 className="text-center text-2xl font-semibold md:text-4xl">
+                    Your library seems to be empty
+                  </h1>
+                </div>
+              </div>
+            )}
+          </div>
         ) : (
           <div className="flex justify-center">
-            <div className="flex flex-col items-center gap-4 py-12 text-white">
+            <div className="flex flex-col items-center gap-4 py-12">
               <h1 className="text-lg md:text-2xl">
                 You must be logged in to access library
               </h1>
